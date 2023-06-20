4b6340ba0ee9afdca0fd7d4466c154cd544d93bc
Mnishthakur <102964888+Mnishthakur@users.noreply.github.com>
2023-06-20 13:45:03 +0530

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,16 @@
+namespace Stack_and_queue_Problems;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            Stack stack = new Stack();
+            stack.Push(70);
+            stack.Push(30);
+            stack.Push(56);
+
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Pop());
+        }
+    }
